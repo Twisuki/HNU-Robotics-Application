9b8f7028c3f337aa924fb53d2e8438bc41f92a92
Twisuki <Twisuki@outlook.com>
2025-05-23 22:23:22 +0800

    替换图片

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,8 +6,8 @@ export default defineUserConfig({
   base: "/",
 
   lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  title: "学生手册",
+  description: "湖南大学机器人学院学生手册",
 
   theme,
 
