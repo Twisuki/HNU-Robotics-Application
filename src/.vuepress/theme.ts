e44857ba3f2772176a454a8ec4d5dda0d4cf396f
Twisuki <Twisuki@outlook.com>
2025-05-24 14:44:04 +0800

    主页"其他类型的文章"

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -123,9 +123,26 @@ export default hopeTheme({
     // sandpack: true,
   },
 
+  // 博客主页文章设置
+  blogLocales: {
+    "index": "索引",
+    "stars": "精选",
+  },
+
   // 在这里配置主题提供的插件
   plugins: {
-    blog: true,
+    blog: {
+      type: [
+        {
+          key: "index",
+          filter: (page) => page.frontmatter.type === "index",
+        },
+        {
+          key: "stars",
+          filter: (page) => page.frontmatter.star === "true"
+        }
+      ],
+    },
 
     // 启用之前需安装 @waline/client
     // 警告: 这是一个仅供演示的测试服务，在生产环境中请自行部署并使用自己的服务！
